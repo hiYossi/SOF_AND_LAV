1a361ac6c19e534c0ae8914979e4a14e9a6369fa
Sofer <omer.sofer.46@gmail.com>
2026-04-05 19:38:31 +0300

    lahav_homo_bathat

--- lahav_gay.py
+++ lahav_gay.py
@@ -1,2 +1,8 @@
-while True:
-    print("sofer_gay")
+def lahav_homo_multiplyer(num):
+    for i in range(1,num+1):
+        print("lahav is gay")
+
+
+lahav_homo_multiplyer(10000000000)
+"""while True:
+    print("sofer_gay")"""
